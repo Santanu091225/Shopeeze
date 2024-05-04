2fac5424beb4dfbc60d6e6e3aa245055d2bc2ce2
Santanu Pramanik <santanu091225@gmail.com>
2024-05-04 15:20:28 +0530

    Update settings.py

--- Shopeeze/settings.py
+++ Shopeeze/settings.py
@@ -104,20 +104,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mailshopeeze002@gmail.com'
-EMAIL_HOST_PASSWORD = 'bceubatkjgzwmxom'
-
-
-
-
 RZP_KEY_ID = 'YOUR_RAZORPAY_KEY_ID'
 RZP_SECRET_KEY = 'YOUR_RAZORPAY_SECRET_KEY'
 
